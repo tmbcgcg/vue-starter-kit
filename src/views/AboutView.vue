c0cc54fcc00b22874423fd9608b38f250138db00
Tanmay Bhide <Tanmay_Bhide@capgroup.com>
2023-10-19 15:41:15 -0700

    Simple template commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1 class="text-xl font-large text-white">This is an about page</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.about {
+  @apply lg:min-h-screen lg:flex lg:items-center;
+  h1 {
+    @apply text-xl font-medium text-white;
+  }
+}
+</style>
